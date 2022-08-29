19a0dd9bb2b02da3c4f2aaab9b370df3ddb6dd1c
Eugene Noble <71329328+Eugenenoble2005@users.noreply.github.com>
2022-08-29 20:49:59 +0100

    new commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        inventory::factory()->count(30)->create();
+        inventory::factory()->count(5)->create();
     }
 }
